41d3d3c97cd6a030ad7c229826f31c889a0872a7
Neil Harrison <ntharrison12@gmail.com>
2022-01-14 11:48:37 +0000

    Added a turtle sprite and flipped up/down moves

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 window = tk.Tk()
 
@@ -7,24 +8,34 @@ coords = [150, 150]
 canvas = tk.Canvas(window, bg="white", height=300, width=300)
 canvas.grid(column=0, row=0, columnspan=4)
 
+#Turtle sprite loading
+image = Image.open("turtle.png")
+resizeimage = image.resize((30,int(30*image.size[0]/image.size[1])))
+loadTurtle = ImageTk.PhotoImage(resizeimage)
+turtle = canvas.create_image(coords[0]-15,coords[1]-resizeimage.size[1]/2,anchor=tk.NW, image=loadTurtle)
 
 def move_up():
-    canvas.create_line(coords[0], coords[1], coords[0], coords[1] + 10, fill="black", width=1)
-    coords[1] += 10
+    #up is down!
+    canvas.create_line(coords[0], coords[1], coords[0], coords[1] - 10, fill="black", width=1)
+    canvas.move(turtle,0,-10)
+    coords[1] -= 10
 
 
 def move_down():
-    canvas.create_line(coords[0], coords[1], coords[0], coords[1] - 10, fill="black", width=1)
-    coords[1] -= 10
+    canvas.create_line(coords[0], coords[1], coords[0], coords[1] + 10, fill="black", width=1)
+    canvas.move(turtle,0,10)
+    coords[1] += 10
 
 
 def move_left():
     canvas.create_line(coords[0], coords[1], coords[0] - 10, coords[1], fill="black", width=1)
+    canvas.move(turtle,-10,0)
     coords[0] -= 10
 
 
 def move_right():
     canvas.create_line(coords[0], coords[1], coords[0] + 10, coords[1], fill="black", width=1)
+    canvas.move(turtle,10,0)
     coords[0] += 10
 
 def turn_left():
