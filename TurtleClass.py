3ef7dd0076d314ea78ab6ee53a4b5ca25c2a097c
neil <ntharrison12@gmail.com>
2022-01-15 12:47:45 +0000

    Added keyboard commands and reset function

--- TurtleClass.py
+++ TurtleClass.py
@@ -12,6 +12,7 @@ class Turtle:
         self.load_sprite_canvas()
         self.pen = True
         self.colour = '#000000'
+        self.angle = 0
 
     def load_sprite_file(self, filename="turtle.png"):
         image = Image.open(filename)
@@ -24,16 +25,17 @@ class Turtle:
     def rotate(self,angle):
         self.canvas.delete(self.turtle_sprite)
         self.resizeimage = self.resizeimage.rotate(angle)
+        self.angle+=angle
         self.load_sprite_canvas()
 
     def move(self, dir, dist=10):
-        if dir == "Up":
+        if dir == "Up" or dir=="w":
             coord_change = np.array([0,-1])
-        elif dir == "Down":
+        elif dir == "Down" or dir=="s":
             coord_change = np.array([0,1])
-        elif dir == "Left":
+        elif dir == "Left" or dir=="a":
             coord_change = np.array([-1,0])
-        elif dir =="Right":
+        elif dir =="Right" or dir=="d":
             coord_change = np.array([1,0])
 
         coord_change = coord_change*dist
@@ -43,7 +45,14 @@ class Turtle:
         self.canvas.move(self.turtle_sprite,coord_change[0],coord_change[1])
         self.coords += coord_change
 
-        
+    def reset(self):
+        #slightly off centre but not a big issue
+        self.coords=np.array([self.canvas.winfo_height()/2, self.canvas.winfo_width()/2])
+        self.canvas.delete("all")
+        print(self.angle)
+        self.rotate(-self.angle)
+        #self.load_sprite_canvas() loaded again in rotate
+
 
 
     def pen_on_off(self):
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import TurtleClass as tc #tc is a bit close to tk
+from functools import partial
 
 window = tk.Tk()
 window.title("Turtle Sim")
@@ -9,7 +10,23 @@ canvas.grid(column=0, row=0, columnspan=4)
 
 turtle = tc.Turtle(canvas)
 
-tk.Button(window, text="↑", command=lambda : turtle.move("Up")).grid(column=0, row=1)
+def keypress_manager(event):
+    if event.keysym == "Left" or event.keysym == "Right" or event.keysym == "Up" or event.keysym == "Down" or event.keysym == "w" or event.keysym == "a" or event.keysym == "s" or event.keysym == "d":
+        turtle.move(event.keysym)
+    elif event.keysym == "e":
+        turtle.rotate(-90)
+    elif event.keysym == "q":
+        turtle.rotate(90)
+    elif event.keysym == "space":
+        turtle.pen_on_off()
+    elif event.keysym == "r":
+        turtle.reset()
+    elif event.keysym =="Escape":
+        window.destroy()
+    
+
+
+tk.Button(window, text="↑", command=partial(turtle.move,"Up")).grid(column=0, row=1)
 tk.Button(window, text="↓", command=lambda : turtle.move("Down")).grid(column=1, row=1)
 tk.Button(window, text="←", command=lambda : turtle.move("Left")).grid(column=2, row=1)
 tk.Button(window, text="→", command=lambda : turtle.move("Right")).grid(column=3, row=1)
@@ -17,6 +34,8 @@ tk.Button(window, text="↰", command=lambda : turtle.rotate(90)).grid(column=1,
 tk.Button(window, text="↱", command=lambda : turtle.rotate(-90)).grid(column=2, row=2)
 tk.Button(window, text="pen", command=turtle.pen_on_off).grid(column=0, row=2)
 tk.Button(window, text="colour", command=turtle.pick_colour).grid(column=3, row=2)
+
+window.bind('<Key>',keypress_manager)
 
 
 window.mainloop()
