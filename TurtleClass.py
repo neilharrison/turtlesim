c30ec3ca7b5e05f5b9c9ee7550ad1496c2d08c99
neil <ntharrison12@gmail.com>
2022-01-15 20:54:30 +0000

    Obstacles! Creating and removing using mouse, turtle cannot crash into obstacles

--- TurtleClass.py
+++ TurtleClass.py
@@ -13,6 +13,7 @@ class Turtle:
         self.pen = True
         self.colour = '#000000'
         self.angle = 0
+        self.obs_flag = False
 
     def load_sprite_file(self, filename="turtle.png"):
         image = Image.open(filename)
@@ -20,7 +21,7 @@ class Turtle:
     
     def load_sprite_canvas(self):
         self.loadTurtle = ImageTk.PhotoImage(self.resizeimage)
-        self.turtle_sprite = self.canvas.create_image(self.coords[0]-15,self.coords[1]-self.resizeimage.size[1]/2,anchor=tk.NW, image=self.loadTurtle)
+        self.turtle_sprite = self.canvas.create_image(self.coords[0]-15,self.coords[1]-self.resizeimage.size[1]/2,anchor=tk.NW, image=self.loadTurtle,tag="turtle")
     
     def rotate(self,angle):
         self.canvas.delete(self.turtle_sprite)
@@ -37,12 +38,19 @@ class Turtle:
             coord_change = np.array([-1,0])
         elif dir =="Right" or dir=="d":
             coord_change = np.array([1,0])
-
+        
         coord_change = coord_change*dist
+
+        overlaps = list(self.canvas.find_overlapping(self.coords[0], self.coords[1], self.coords[0]+coord_change[0],self.coords[1]+coord_change[1]))
+        crash = False
+        for i in overlaps:
+            if self.canvas.itemconfig(i)["tags"][-1] == "obstacle" or self.canvas.itemconfig(i)["tags"][-1] == "obstacle current":
+                crash = True
+                
         buffer = 2
-        if (buffer < self.coords[0]+coord_change[0] < self.canvas.winfo_width()-buffer) and (buffer < self.coords[1]+coord_change[1] < self.canvas.winfo_height()-buffer):
+        if not crash and (buffer < self.coords[0]+coord_change[0] < self.canvas.winfo_width()-buffer) and (buffer < self.coords[1]+coord_change[1] < self.canvas.winfo_height()-buffer):
             if self.pen:
-                self.canvas.create_line(self.coords[0], self.coords[1], self.coords[0]+coord_change[0], self.coords[1]+coord_change[1], fill=self.colour, width=1)
+                self.canvas.create_line(self.coords[0], self.coords[1], self.coords[0]+coord_change[0], self.coords[1]+coord_change[1], fill=self.colour, width=1, tag="line")
             self.canvas.move(self.turtle_sprite,coord_change[0],coord_change[1])
             self.coords += coord_change
 
@@ -53,7 +61,23 @@ class Turtle:
         self.rotate(-self.angle)
         #self.load_sprite_canvas() loaded again in rotate
 
+    def obstacle_mouse(self,xnew,ynew):
+        if not self.obs_flag:
+            self.obstacle_coords = [xnew,ynew]
+            self.obs_flag = True
+        else:
+            self.square_obstacle(self.obstacle_coords[0],self.obstacle_coords[1],xnew,ynew)
+            self.obs_flag = False
+    
+    def obstacle_remove(self,x,y):
+        item = self.canvas.find_closest(x,y)
+        if self.canvas.itemconfig(item)["tags"][-1] == "obstacle" or self.canvas.itemconfig(item)["tags"][-1] == "obstacle current":
+            self.canvas.delete(item)
+        self.obs_flag = False
 
+
+    def square_obstacle(self,x1,y1,x2,y2):
+        self.canvas.create_rectangle(x1,y1,x2,y2,fill="black",tag="obstacle")
 
     def pen_on_off(self):
         self.pen = not self.pen
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 import TurtleClass as tc #tc is a bit close to tk
-from functools import partial
 
 window = tk.Tk()
 window.title("Turtle Sim")
@@ -23,10 +22,15 @@ def keypress_manager(event):
         turtle.reset()
     elif event.keysym =="Escape":
         window.destroy()
+
+def mouse_obs(event):
+    turtle.obstacle_mouse(event.x,event.y)
+def obs_remove(event):
+    turtle.obstacle_remove(event.x,event.y)
     
 
 
-tk.Button(window, text="↑", command=partial(turtle.move,"Up")).grid(column=0, row=1)
+tk.Button(window, text="↑", command=lambda : turtle.move("Up")).grid(column=0, row=1)
 tk.Button(window, text="↓", command=lambda : turtle.move("Down")).grid(column=1, row=1)
 tk.Button(window, text="←", command=lambda : turtle.move("Left")).grid(column=2, row=1)
 tk.Button(window, text="→", command=lambda : turtle.move("Right")).grid(column=3, row=1)
@@ -36,6 +40,7 @@ tk.Button(window, text="pen", command=turtle.pen_on_off).grid(column=0, row=2)
 tk.Button(window, text="colour", command=turtle.pick_colour).grid(column=3, row=2)
 
 window.bind('<Key>',keypress_manager)
-
+window.bind('<Button-1>', mouse_obs)
+window.bind('<Double-Button-1>',obs_remove)
 
 window.mainloop()
